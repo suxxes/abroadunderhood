9eae0912fc0306cd9ebe3a8720d9ba56b834be3b
Vladimir Starkov <iamstarkov@gmail.com>
2015-11-16 19:26:50 +0100

    post false

--- authors.js
+++ authors.js
@@ -1,4 +1,4 @@
 export default [
   // post: false
-  { username: 'suxxes',  start: '15 Nov 2015', first: '666309260360474624' },
+  { username: 'suxxes',  start: '15 Nov 2015', first: '666309260360474624', post: false },
 ];
