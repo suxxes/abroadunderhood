b359123464bd3caf3c47a08699b1a83d69898bcb
Vladimir Starkov <iamstarkov@gmail.com>
2015-11-27 01:36:41 +0100

    fix media dump

--- update.js
+++ update.js
@@ -3,6 +3,7 @@ import { outputFile } from 'fs-extra';
 import { isEmpty, concat, reverse, last, dissoc, map, head } from 'ramda';
 import moment from 'moment';
 import dec from 'bignum-dec';
+import { sync as rm } from 'rimraf';
 
 import authors from './authors';
 
@@ -36,6 +37,7 @@ getInfo(tokens, 'abroadunderhood', (err, info) => {
   saveAuthorArea(username, 'info', info);
 });
 
+rm(`./dump/images/${username}*`);
 saveMedia(tokens, 'abroadunderhood', username, (err, media) => {
   if (err) throw err;
   saveAuthorArea(username, 'media', media);
