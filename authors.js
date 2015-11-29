a60f5e1187aa7d4f2df7f6658f4fcf41124e0e6c
Fedor <unno@unno.ru>
2015-11-29 20:54:24 +0300

    author - ido_q - close

--- authors.js
+++ authors.js
@@ -1,4 +1,4 @@
 export default [
   // post: false
-  { username: 'ido_q',  start: '23 Nov 2015', first: '668525310783045632', post: false },
+  { username: 'ido_q',  start: '23 Nov 2015', first: '668525310783045632' },
 ];
