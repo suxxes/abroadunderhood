766886abba80ab29bed71e93b990fe4739545266
Fedor <unno@unno.ru>
2015-12-04 13:14:50 +0300

    more archive styling

--- helpers/author-render.js
+++ helpers/author-render.js
@@ -54,7 +54,7 @@ const renderVideo = url => {
   var matches = regexp.exec(url);
 
   if (matches) {
-    return '<div class="embed-responsive embed-responsive-16by9"><iframe src="//www.youtube.com/embed/' + matches[2] + '" width="720" height="' + (720 * (9 / 16)) + '" class="embed-responsive-item"></iframe></div>';
+    return '<p class="embed-responsive embed-responsive-16by9"><iframe src="//www.youtube.com/embed/' + matches[2] + '" width="720" height="' + (720 * (9 / 16)) + '" class="embed-responsive-item"></iframe></p>';
   }
 }
 
