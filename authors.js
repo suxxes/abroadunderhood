46fd9b384ef279a4ad806b6f955e79275d9fe863
Fedor <unno@unno.ru>
2015-12-07 00:54:31 +0300

    author - damirca - close

--- authors.js
+++ authors.js
@@ -1,5 +1,5 @@
 export default [
   // post: false
-  { username: 'damirca',  start: '30 Nov 2015', first: '671225043679080448', post: false },
+  { username: 'damirca',  start: '30 Nov 2015', first: '671225043679080448' },
   { username: 'ido_q',  start: '23 Nov 2015', first: '668525310783045632' },
 ];
