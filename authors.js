650f55ad0d8aa192c422b4e5382fd8bfcbb5d000
Fedor <unno@unno.ru>
2015-12-14 01:44:51 +0300

    author - swither - close

--- authors.js
+++ authors.js
@@ -1,6 +1,6 @@
 export default [
   // post: false
-  { username: 'swither',  start: '07 Dec 2015', first: '673792948782235648', post: false },
+  { username: 'swither',  start: '14 Dec 2015', first: '673792948782235648' },
   { username: 'damirca',  start: '30 Nov 2015', first: '671225043679080448' },
   { username: 'ido_q',  start: '23 Nov 2015', first: '668525310783045632' },
 ];
