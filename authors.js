6e33a3fd77f524781503bc6bf20f1dfebbe2fd6f
Fedor <unno@unno.ru>
2015-12-27 23:46:36 +0300

    author - kipruss - close

--- authors.js
+++ authors.js
@@ -1,6 +1,7 @@
 export default [
   // post: false
-  { username: 'kipruss',  start: '21 Dec 2015', first: '678858099478814720', post: false },
+  // { username: 'nick_jastix',  start: '28 Dec 2015', first: '', post: false },
+  { username: 'kipruss',  start: '21 Dec 2015', first: '678858099478814720' },
   { username: 'freiksenet_ru',  start: '14 Dec 2015', first: '676296621496344576' },
   { username: 'swither',  start: '07 Dec 2015', first: '673792948782235648' },
   { username: 'damirca',  start: '30 Nov 2015', first: '671225043679080448' },
