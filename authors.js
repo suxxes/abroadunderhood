663b13e47e56f242a27653ab8fe3b08c21f451fb
Fedor <unno@unno.ru>
2015-12-28 10:13:33 +0300

    author - nick_jastix

--- authors.js
+++ authors.js
@@ -1,6 +1,6 @@
 export default [
   // post: false
-  // { username: 'nick_jastix',  start: '28 Dec 2015', first: '', post: false },
+  { username: 'nick_jastix',  start: '28 Dec 2015', first: '681369482112143360', post: false },
   { username: 'kipruss',  start: '21 Dec 2015', first: '678858099478814720' },
   { username: 'freiksenet_ru',  start: '14 Dec 2015', first: '676296621496344576' },
   { username: 'swither',  start: '07 Dec 2015', first: '673792948782235648' },
