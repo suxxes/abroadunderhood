898b2200fa628c8bf20b9c0d7a0fe99736ea24e5
Fedor <unno@unno.ru>
2016-05-09 00:11:05 +0200

    author - mamezito - close

--- authors.js
+++ authors.js
@@ -1,6 +1,7 @@
 export default [
   // post: false
-  { username: 'mamezito', start: '02 Mar 2016', first: '726876827331993604', post: false },
+  // { username: 'toivonens', start: '09 May 2016', first: '726876827331993604', post: false },
+  { username: 'mamezito', start: '02 May 2016', first: '726876827331993604' },
   { username: 'svetosil', start: '25 Apr 2016', first: '724493220982321152' },
   { username: 'interviewfit1', start: '18 Apr 2016', first: '721882184664137728' },
   { username: 'gwenywere', start: '11 Apr 2016', first: '719407353985122305' },
