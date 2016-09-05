2e3c6b3db2a1d0701dc9668cce3e9147bc68cdf6
Fedor <unno@unno.ru>
2016-09-05 10:01:35 +0300

    author - yirsky - close

--- authors.js
+++ authors.js
@@ -2,7 +2,8 @@ import authorId from './helpers/author-id';
 
 export default authorId([
   // post: false
-  { username: 'yirsky', start: '29 Aug 2016', first: '770023237434630144', post: false },
+  // { username: 'naservere', start: '5 Sep 2016', first: '', post: false },
+  { username: 'yirsky', start: '29 Aug 2016', first: '770023237434630144' },
   { username: 'YahNNy', start: '22 Aug 2016', first: '767689930059476992' },
   { username: 'artzonemsk', start: '15 Aug 2016', first: '765210192149618692' },
   { username: 'achkasov', start: '8 Aug 2016', first: '762416870725066752' },
