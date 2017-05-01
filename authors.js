81760f5e9dde87f6475e4b544ea319bda1d3fc89
Fedor <unno@unno.ru>
2017-05-01 11:33:26 +0300

    author - cupofteanyone

--- authors.js
+++ authors.js
@@ -2,7 +2,7 @@ import authorId from './helpers/author-id';
 
 export default authorId([
   // post: false
-  // { username: 'cupofteanyone', start: '1 May 2017', first: '', post: false },
+  { username: 'cupofteanyone', start: '1 May 2017', first: '858959846556696576', post: false },
   { username: 'lonefur', start: '24 Apr 2017', first: '856582262795755520' },
   { username: 'iFORA', start: '17 Apr 2017', first: '853974816881221633' },
   { username: 'vradnik', start: '10 Apr 2017', first: '851375403348488192' },
