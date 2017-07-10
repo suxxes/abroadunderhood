17a4610218470c6eec7b187db97d0990e3336598
Fedor <unno@unno.ru>
2017-07-10 16:04:05 +0300

    Author - biomehanik

--- authors.js
+++ authors.js
@@ -2,7 +2,7 @@ import authorId from './helpers/author-id';
 
 export default authorId([
   // post: false
-  // { username: 'biomehanik', start: '10 Jul 2017', first: '', post: false },
+  { username: 'biomehanik', start: '10 Jul 2017', first: '884395935962419201', post: false },
   { username: 'anevseev', start: '3 Jul 2017', first: '881819986305720322' },
   { username: 'den_andra_sidan', start: '26 Jun 2017', first: '879106972804677633' },
   { username: 'alex_fadeev', start: '19 Jun 2017', first: '876667453442588672' },
