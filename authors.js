e6a5842624dea0ab32b48dcbaf4f5074236bf795
Fedor <unno@unno.ru>
2017-09-25 09:53:20 +0300

    Author - alestainer_fox

--- authors.js
+++ authors.js
@@ -2,7 +2,8 @@ import authorId from './helpers/author-id';
 
 export default authorId([
   // post: false
-  // { username: 'alestainer_fox', start: '18 Sep 2017', first: '', post: false },
+  // { username: 'health_vancity', start: '25 Sep 2017', first: '', post: false },
+  { username: 'alestainer_fox', start: '18 Sep 2017', first: '909936704492003328', post: false },
   { username: 'MsMentret', start: '11 Sep 2017', first: '907213232854781953' },
   { username: 'zezechai', start: '4 Sep 2017', first: '904575385064615936' },
   { username: 'grayraw', start: '28 Aug 2017', first: '902144914858819586' },
