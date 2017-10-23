3bbb87f20e8bebec2cbd52bcd81b2db89bac3d7d
Fedor <unno@unno.ru>
2017-10-23 19:41:19 +0300

    Author - Mezana_v

--- authors.js
+++ authors.js
@@ -2,7 +2,8 @@ import authorId from './helpers/author-id';
 
 export default authorId([
   // post: false
-  // { username: 'Mezana_v', start: '16 Oct 2017', first: '', post: false },
+  // { username: 'ilyakharlamov', start: '23 Oct 2017', first: '', post: false },
+  { username: 'Mezana_v', start: '16 Oct 2017', first: '919821923139358720' },
   { username: 'yourmummy_cy', start: '9 Oct 2017', first: '917305503000166403' },
   { username: 'kosinskaya', start: '2 Oct 2017', first: '914781475505102849' },
   { username: 'anna_redhot', start: '25 Sep 2017', first: '912351106012921856' },
