ddb477b5425133df1dc8d53b028e108fac7f9650
Fedor <unno@unno.ru>
2017-10-30 16:51:14 +0300

    Author - bittabola

--- authors.js
+++ authors.js
@@ -2,7 +2,7 @@ import authorId from './helpers/author-id';
 
 export default authorId([
   // post: false
-  // { username: 'bittabola', start: '30 Oct 2017', first: '', post: false },
+  { username: 'bittabola', start: '30 Oct 2017', first: '924995518719131648', post: false },
   { username: 'ilyakharlamov', start: '23 Oct 2017', first: '922557410295861250' },
   { username: 'Mezana_v', start: '16 Oct 2017', first: '919821923139358720' },
   { username: 'yourmummy_cy', start: '9 Oct 2017', first: '917305503000166403' },
