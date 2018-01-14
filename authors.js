c85f35aeec21ac0339bedb7a667b9d5422c79b68
Fedor <unno@unno.ru>
2018-01-15 01:14:17 +0300

    Author - Sanatorievna - close

--- authors.js
+++ authors.js
@@ -2,7 +2,8 @@ import authorId from './helpers/author-id';
 
 export default authorId([
   // post: false
-  { username: 'Sanatorievna', start: '8 Jan 2018', first: '950244668591222790', post: false },
+  // { username: 'alwxtwi', start: '15 Jan 2018', first: '', post: false },
+  { username: 'Sanatorievna', start: '8 Jan 2018', first: '950244668591222790' },
   { username: 'anaspektor', start: '1 Jan 2018', first: '947658527488856064' },
   { username: 'maxhocom', start: '25 Dec 2017', first: '945087278006726656' },
   { username: 'cellular_bud', start: '18 Dec 2017', first: '942614077020483584' },
