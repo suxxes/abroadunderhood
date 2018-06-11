8aa4b2b06fdc26e6ceb63ab31d4b6dfa1214b6e1
Fedor <unno@unno.ru>
2018-06-11 09:48:36 +0200

    .

--- authors.js
+++ authors.js
@@ -2,7 +2,7 @@ import authorId from './helpers/author-id';
 
 export default authorId([
   // post: false
-  { username: 'NikoStolz', start: '11 Jun 2018', first: '1006051345546317824', post: false },
+  // { username: 'NikoStolz', start: '11 Jun 2018', first: '1006051345546317824', post: false },
   { username: 'stay_positive', start: '4 Jun 2018', first: '1003433268329320448' },
   { username: 'SOlga16', start: '28 May 2018', first: '1000872862507585537' },
   { username: 'AlenaPolska', start: '21 May 2018', first: '998434225996095488' },
