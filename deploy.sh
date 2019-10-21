40b30f566dc4e528010c7a895dffc33eb09ccbc7
Fedor <unno@unno.me>
2019-10-21 12:02:54 +0300

    Deploy with NVM

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+source ~/.zshrc
+
+echo "store current Node version"
+CURRENT_NODE="$(nvm current)"
+echo "${CURRENT_NODE}"
+
+echo "switch Node version to v11"
+nvm use 11
+
 echo "set git environment"
 git remote rm origin
 git remote add origin https://suxxes:${GITHUB_TOKEN}@github.com/suxxes/abroadunderhood.git
@@ -15,3 +24,6 @@ git push origin master &>/dev/null
 
 echo "build'n'deploy"
 npm run deploy
+
+echo "switch Node version to ${CURRENT_NODE}"
+echo "$(nvm ${CURRENT_NODE})"
