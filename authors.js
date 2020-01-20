1c746a6590fdf6b20c86696fcb9bf5380f4fc288
Fedor <unno@unno.me>
2020-01-20 23:56:31 +0300

    Author - KramaWerty

--- authors.js
+++ authors.js
@@ -2,7 +2,7 @@ import authorId from './helpers/author-id';
 
 export default authorId([
   // post: false
-  // { username: 'KramaWerty', start: '20 Jan 2020', first: '', post: false },
+  { username: 'KramaWerty', start: '20 Jan 2020', first: '1219330766657728514', post: false },
   { username: 'MTussupova', start: '13 Jan 2020', first: '1216658712103989248' },
   { username: 'rembish', start: '6 Jan 2020', first: '1214265698714824709' },
   { username: 'corsar_cv', start: '23 Dec 2019', first: '1209102401938046976' },
