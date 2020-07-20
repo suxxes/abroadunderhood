f4a8381795be7e57096ee9ee9f722a687c1761d7
Fedor <unno@unno.me>
2020-07-20 14:26:18 +0300

    Author - Lik04ka

--- authors.js
+++ authors.js
@@ -3,6 +3,7 @@ import authorId from './helpers/author-id';
 export default authorId([
   // post: false
   // { username: '', start: '11 May 2020', first: '', post: false },
+  { username: 'Lik04ka', start: '13 Jul 2020', first: '1282750920380559362' },
   { username: 'froekenbock', start: '6 Jul 2020', first: '1280082192740335619' },
   { username: 'Vinni97248909', start: '29 Jun 2020', first: '1277659091373035520' },
   { username: 'pe4alnij_angel', start: '22 Jun 2020', first: '1275043653149392898' },
