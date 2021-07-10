e6477bed4d5a0e7cdbd8c711703eb7eb0133bef7
Fedor <unno@unno.me>
2021-07-10 08:19:01 +0300

    Author - strydel

--- authors.js
+++ authors.js
@@ -3,7 +3,7 @@ import authorId from './helpers/author-id';
 export default authorId([
   // post: false
   // { username: '', start: '11 May 2020', first: '', post: false },
-  // { username: 'strydel', start: '5 July 2021', first: '1411988746354839561' },
+  { username: 'strydel', start: '5 July 2021', first: '1411988746354839561', post: false },
   { username: 'ProstoRudi', start: '28 June 2021', first: '1409486800209408003' },
   { username: 'Val_Scherbak', start: '21 June 2021', first: '1406947101976809474' },
   { username: 'Dasha_Ell', start: '24 May 2021', first: '1396916930557186048' },
